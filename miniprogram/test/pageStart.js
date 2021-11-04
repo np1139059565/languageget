3f9a5e4f65b9f857455f9fb534cf81bec1e03690
np1139059565 <1139059565@qq.com>
2021-11-04 19:01:07 +0800

    https://github.com/np1139059565/languageget/issues/10#issue-1042939649

--- miniprogram/test/pageStart.js
+++ miniprogram/test/pageStart.js
@@ -1,54 +1,54 @@
-
-class WxPage {
-    constructor (options) {
-        this.data = options.data || {}
-        Object.keys(options).map(key=> {
-            if (key !== 'data') {
-                this[key] = options[key]
-            }
-        })
-    }
-
-    setData (newData, cb) {
-        setTimeout(() => {
-            Object.assign(this.data, newData)
-            cb && cb()
-        })
-    }
-}
-class WXApp{
-    constructor (options) {
-        this.data = options.data || {}
-        Object.keys(options).map(key=> {
-            if (key !== 'data') {
-                this[key] = options[key]
-            }
-        })
-    }
-
-    setData (newData, cb) {
-        setTimeout(() => {
-            Object.assign(this.data, newData)
-            cb && cb()
-        })
-    }
-}
-
-function App(options){
-    global._wx.app = new WXApp(options)
-}
-function getApp(){
-    return require("../app.js")
-}
-function Page (options) {
-    global._wx.page = new WxPage(options)
-}
-
-
-global._wx = {
-    app: null,
-    page: null
-}
-global.Page = Page
-// global.App = App
-global.getApp = getApp//() => global._wx.app
+//
+// class WxPage {
+//     constructor (options) {
+//         this.data = options.data || {}
+//         Object.keys(options).map(key=> {
+//             if (key !== 'data') {
+//                 this[key] = options[key]
+//             }
+//         })
+//     }
+//
+//     setData (newData, cb) {
+//         setTimeout(() => {
+//             Object.assign(this.data, newData)
+//             cb && cb()
+//         })
+//     }
+// }
+// class WXApp{
+//     constructor (options) {
+//         this.data = options.data || {}
+//         Object.keys(options).map(key=> {
+//             if (key !== 'data') {
+//                 this[key] = options[key]
+//             }
+//         })
+//     }
+//
+//     setData (newData, cb) {
+//         setTimeout(() => {
+//             Object.assign(this.data, newData)
+//             cb && cb()
+//         })
+//     }
+// }
+//
+// function App(options){
+//     global._wx.app = new WXApp(options)
+// }
+// function getApp(){
+//     return require("../app.js")
+// }
+// function Page (options) {
+//     global._wx.page = new WxPage(options)
+// }
+//
+//
+// global._wx = {
+//     app: null,
+//     page: null
+// }
+// global.Page = Page
+// // global.App = App
+// global.getApp = getApp//() => global._wx.app
